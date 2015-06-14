b227a96a8cc2744f5204707e52386a51fee8d03e
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-06-14 21:16:53 +0100

    Added functionality to complete tasks

--- js/toDoListController.js
+++ js/toDoListController.js
@@ -5,13 +5,19 @@ toDo.controller('toDoController', [function() {
     if (self.list === undefined) {
       self.list = [];
     }
-    var item = {completed: false, task: self.toDoItem}
+    var item = {
+      completed: false,
+      task: self.toDoItem
+    }
     self.list.push(item)
   };
 
-  self.completeItem = function() {
-      self.list[0].completed = true
-      
+  self.completeItem = function(task) {
+    for (i = 0; i < self.list.length; i++) {
+      if (self.list[i].task === task) {
+        self.list[i].completed = true;
+      }
+    }
   }
 
 }]);
--- test/toDoListController.spec.js
+++ test/toDoListController.spec.js
@@ -2,6 +2,10 @@ describe('toDoController', function() {
   beforeEach(module('toDo'));
 
   var ctrl;
+  var addTask = function(text) {
+    ctrl.toDoItem = text;
+    ctrl.addItem();
+  }
 
   beforeEach(inject(function($controller) {
     ctrl = $controller('toDoController');
@@ -16,25 +20,28 @@ describe('toDoController', function() {
   });
 
   it('adding an item to the list', function() {
-    ctrl.toDoItem = 'write more tests';
-    ctrl.addItem();
+      addTask('write more tests');
     expect(ctrl.list[0].task).toEqual('write more tests');
   });
 
   it('can add two items to the list', function() {
-    ctrl.toDoItem = 'write more tests';
-    ctrl.addItem();
-    ctrl.toDoItem = 'write even more tests';
-    ctrl.addItem();
+     addTask('write more tests');
+    addTask('write better tests');
     expect(ctrl.list.length).toEqual(2);
   });
 
   it('can mark a task as completed', function() {
-      ctrl.toDoItem = 'filter tasks';
-      ctrl.addItem();
-      ctrl.completeItem();
+      addTask('complete tasks')
+      ctrl.completeItem('complete tasks');
       expect(ctrl.list[0].completed).toBe(true);
       });
+
+  it('can mark the correct task as completed', function(){
+    addTask('complete tasks');
+    addTask('add tests');
+    ctrl.completeItem('add tests');
+    expect(ctrl.list[1].completed).toBe(true);
+  });
 
 
 });
